71703ddf80301c904d38dacff7b2fda0031f9b8a
Maady <vlad030911@gmail.com>
2024-03-14 11:53:00 +0300

    change problems

--- src/features/MainUser/index.ts
+++ src/features/MainUser/index.ts
@@ -1,9 +1,9 @@
-import User from "./store/MainUser.ts";
 import {login} from "./api/Login/login.ts";
-import {registration} from "@/features/User/api/Registration/Registration.ts";
+import MainUser from "@/features/MainUser/store/MainUser.ts";
+import {registration} from "@/features/MainUser/api/Registration/Registration.ts";
 
 export {
-    User,
+    MainUser,
     login,
     registration,
 }
