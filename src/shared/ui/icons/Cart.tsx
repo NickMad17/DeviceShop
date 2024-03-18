c8c48296cee35db8cb08f07c9ab0b3d5d0ca9fc7
Maady <vlad030911@gmail.com>
2024-03-18 04:04:36 +0300

    add icon

--- src/shared/ui/icons/Cart.tsx
+++ src/shared/ui/icons/Cart.tsx
@@ -3,10 +3,11 @@ import {Button} from "@/shared/shadcnui/ui/button.tsx";
 
 const Cart = () => {
     return (
-            <Button className='border-none rounded-xl' variant="outline" size="icon">
-                <ShoppingCart className='w-5 h-5'/>
-                <span className="sr-only">Toggle theme</span>
-            </Button>
+        <Button className='border-none rounded-xl relative' variant="outline" size="icon">
+            <ShoppingCart className='w-5 h-5'/>
+            <span className="sr-only">Toggle theme</span>
+            <div className='absolute top-[-2px] right-0 z-10 w-5 h-5 rounded-full bg-primary text-[11px]'>1</div>
+        </Button>
     );
 };
 
